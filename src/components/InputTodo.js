c9a98d43c25b8654d680e8393f085820d86585b0
Yersel Hurtado <yersel500@gmail.com>
2022-03-21 19:59:15 -0500

    Edit Todo items

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,39 +1,40 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
   state = {
-    title: ""
+    title: '',
   };
 
-  onChange = e => {
+  onChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    if(this.state.title.trim()) {
+    if (this.state.title.trim()) {
       this.props.addTodoProps(this.state.title);
       this.setState({
-        title: "",
+        title: '',
       });
     }
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className='form-container'>
-        <input 
-          type="text" 
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
           className="input-text"
-          placeholder="Add Todo..." 
+          placeholder="Add Todo..."
           value={this.state.title}
-          name='title'
-          onChange={this.onChange} />
+          name="title"
+          onChange={this.onChange}
+        />
         <button className="input-submit">Submit</button>
       </form>
-    )
+    );
   }
 }
-export default InputTodo
+export default InputTodo;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import TodoContainer from "./components/TodoContainer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 ReactDOM.render(<TodoContainer />, document.getElementById('root'));
